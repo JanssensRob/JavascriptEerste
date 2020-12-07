8944046e1055c5cea88e10c9e84a72c107dfad19
Rob <robjanssens93@hotmail.com>
2020-12-07 11:35:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+console.log("Hello Winc Academy");
+var x = 'Rob Janssens';
+console.log(x);
+if (x == 'Rob Janssens');
+console.log(x);
+var wif = 'Rob Janssens';//if deed het niet
+console.log(wif);
+var Number = 4 + 4;
+var String = "4" + "4";
+console.log(Number + String);
+console.log(String);
+
+var Som1 = 15;
+var Som2 = 25;
+var Som3 = 35;
+var Som4 = 45;
+
+console.log(Som1 + Som2);
+console.log(Som4 - Som3);
+console.log(Som4 * Som3);
+console.log(Som4 / Som1);
+console.log(Som4 % Som3);
+
+var Age = 27;
+console.log(typeof Age);
+var Age1 = "27";
+console.log(typeof Age1);
